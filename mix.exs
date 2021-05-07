b5c2e8c74bc71d017f6ad60caf2814c7c593ac13
Michael Davis <mcarsondavis@gmail.com>
2021-05-07 15:29:56 -0500

    use upstream mint

--- mix.exs
+++ mix.exs
@@ -11,21 +11,15 @@ defmodule MintWebSocket.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mint,
-       git: "https://github.com/the-mikedavis/mint.git",
-       branch: "keep-http1-conn-open-on-upgrade-header"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:mint, "~> 1.0"}
     ]
   end
 end
