de7064d2f1506d1b62b8a399af38c0aeead85454
Michael Davis <mcarsondavis@gmail.com>
2021-05-07 14:35:45 -0500

    name messages in test for easier debugging

--- test/mint/web_socket_test.exs
+++ test/mint/web_socket_test.exs
@@ -11,14 +11,14 @@ defmodule Mint.WebSocketTest do
       {:ok, conn} = Mint.HTTP.connect(:http, "echo", 8080)
       req_headers = Mint.WebSocket.build_request_headers()
       {:ok, conn, ref} = Mint.HTTP.request(conn, "GET", "/", req_headers, nil)
-      assert_receive message
+      assert_receive http_get_message
       {:ok, conn, [{:status, ^ref, status}, {:headers, ^ref, resp_headers}, {:done, ^ref}]} =
-        Mint.HTTP.stream(conn, message)
+        Mint.HTTP.stream(conn, http_get_message)
       {:ok, conn, websocket} = Mint.WebSocket.new(conn, ref, status, req_headers, resp_headers)
 
       # receive one message about the request being served
-      assert_receive message
-      {:ok, conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, message)
+      assert_receive request_served_by_message, 1_000
+      {:ok, conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, request_served_by_message)
       assert {:ok, websocket, [{:text, "Request served by " <> _}]} = Mint.WebSocket.decode(websocket, data)
 
       # send the hello world frame
@@ -26,8 +26,8 @@ defmodule Mint.WebSocketTest do
       {:ok, conn} = Mint.HTTP.stream_request_body(conn, ref, data)
 
       # receive another message which is the echo reply to our hello world
-      assert_receive message
-      {:ok, _conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, message)
+      assert_receive hello_world_echo_message
+      {:ok, _conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, hello_world_echo_message)
       assert {:ok, _websocket, [{:text, "hello world"}]} = Mint.WebSocket.decode(websocket, data)
     end
   end
