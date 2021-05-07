7afd2750c31c062a180834213cec0774faba53c1
Michael Davis <mcarsondavis@gmail.com>
2021-05-07 09:44:13 -0500

    draft ideas for new/3 in top-level api

--- lib/mint/web_socket.ex
+++ lib/mint/web_socket.ex
@@ -8,8 +8,18 @@ defmodule Mint.WebSocket do
   @type t :: %__MODULE__{}
   defstruct []
 
+  @spec build_request_headers(Keyword.t()) :: Mint.Types.headers()
   def build_request_headers(_opts \\ []) do
     Utils.random_nonce()
     |> Utils.headers()
+  end
+
+  @spec new(%Mint.HTTP1{} | %Mint.HTTP2{}, pos_integer(), Mint.Types.headers()) ::
+          {:ok, %__MODULE__{}} | {:error, any()}
+  def new(_conn, status, _headers) when status != 101 do
+    {:error, :connection_not_upgrading}
+  end
+
+  def new(conn, status, headers) do
   end
 end
