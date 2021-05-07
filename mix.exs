700d600918ac7419ce17c359954a2dd070417ee5
Michael Davis <mcarsondavis@gmail.com>
2021-05-07 14:31:55 -0500

    use my fork of mint

--- mix.exs
+++ mix.exs
@@ -21,7 +21,9 @@ defmodule MintWebSocket.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:mint, git: "https://github.com/the-mikedavis/mint.git", branch: "keep-http1-conn-open-on-upgrade-header"}
+      {:mint,
+       git: "https://github.com/the-mikedavis/mint.git",
+       branch: "keep-http1-conn-open-on-upgrade-header"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
