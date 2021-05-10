5545d5bfed9afe8de8802e7140b1d3e884e18610
Michael Davis <mcarsondavis@gmail.com>
2021-05-09 19:09:23 -0500

    ExUnit test cases match Autobahn|Testsuite cases

--- test/mint/web_socket/autobahn_test.exs
+++ test/mint/web_socket/autobahn_test.exs
@@ -7,14 +7,13 @@ defmodule Mint.WebSocket.AutobahnTest do
   use ExUnit.Case, async: true
 
   @moduletag :autobahn
+  @moduletag :capture_log
 
-  def do_it(case_number \\ 1) do
-    AutobahnClient.connect("/runCase?case=#{case_number}&agent=Mint")
-    |> AutobahnClient.loop()
-  end
-
-  def update_reports do
-    AutobahnClient.connect("/updateReports?agent=Mint")
-    |> AutobahnClient.loop()
+  # for case_number <- Range.new(1, AutobahnClient.get_case_count()) do
+  for case_number <- 1..20 do
+    test "Autobahn|Testsuite case number #{case_number}" do
+      assert AutobahnClient.run_case(unquote(case_number)) == :ok
+      assert AutobahnClient.get_case_status(unquote(case_number)) == "OK"
+    end
   end
 end
