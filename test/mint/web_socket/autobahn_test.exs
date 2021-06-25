c53593b601c4e087c3888a030edace6e00fdff21
Michael Davis <mcarsondavis@gmail.com>
2021-06-25 15:26:31 -0500

    setup permessage-deflate ask for autobahn suite

--- test/mint/web_socket/autobahn_test.exs
+++ test/mint/web_socket/autobahn_test.exs
@@ -9,6 +9,21 @@ defmodule Mint.WebSocket.AutobahnTest do
   @moduletag :autobahn
   @moduletag :capture_log
 
+  @extensions [
+    {~r"^12\.", [Mint.WebSocket.PerMessageDeflate]},
+    {~r"^13\.",
+     [
+       {Mint.WebSocket.PerMessageDeflate,
+        [client_no_context_takeover: true, client_max_window_bits: true]}
+     ]},
+    {~r/.*/, []}
+  ]
+
+  @test_tags [
+    {~r"^9\.", :performance},
+    {~r"^1(2|3)\.", :compression}
+  ]
+
   setup_all do
     on_exit(&AutobahnClient.update_reports/0)
   end
@@ -17,15 +32,23 @@ defmodule Mint.WebSocket.AutobahnTest do
     for case_number <- Range.new(1, AutobahnClient.get_case_count()) do
       info = Task.await(Task.async(fn -> AutobahnClient.get_case_info(case_number) end))
 
-      if String.starts_with?(info.id, "9.") do
-        @tag :performance
+      if tag =
+           Enum.find_value(@test_tags, fn {regex, tag} -> Regex.match?(regex, info.id) && tag end) do
+        @tag tag
       end
 
       test inspect("case #{info.id} (##{case_number}): #{info.description}", printable_limit: 200) do
-        assert AutobahnClient.run_case(unquote(case_number)) == :ok
+        extensions = extensions_for_case(unquote(info.id))
+        assert AutobahnClient.run_case(unquote(case_number), extensions) == :ok
 
         assert AutobahnClient.get_case_status(unquote(case_number)) in ~w[OK NON-STRICT INFORMATIONAL]
       end
     end
+  end
+
+  defp extensions_for_case(case_id) do
+    Enum.find_value(@extensions, fn {id_regex, extensions} ->
+      Regex.match?(id_regex, case_id) && extensions
+    end)
   end
 end
