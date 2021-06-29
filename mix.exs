40ce243e13281a25c39202a4cc457fb6000d2bf6
Michael Davis <mcarsondavis@gmail.com>
2021-06-29 12:26:08 -0500

    use published mint

--- mix.exs
+++ mix.exs
@@ -28,9 +28,10 @@ defmodule MintWebSocket.MixProject do
 
   defp deps do
     [
-      {:mint,
-       git: "https://github.com/elixir-mint/mint.git",
-       ref: "488a6ba5fd418a52f697a8d5f377c629ea96af92"},
+      {:mint, "~> 1.0"},
+      # {:mint,
+      #  git: "https://github.com/elixir-mint/mint.git",
+      #  ref: "488a6ba5fd418a52f697a8d5f377c629ea96af92"},
       {:ex_doc, "~> 0.24", only: [:dev], runtime: false},
       {:castore, ">= 0.0.0", only: [:dev]},
       {:jason, ">= 0.0.0", only: [:dev, :test]},
