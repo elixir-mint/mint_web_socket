94585a52963aa2483eeed7c4845da88445c3f1d2
Michael Davis <mcarsondavis@gmail.com>
2021-11-04 16:21:20 -0500

    fix usage in examples

--- examples/echo.exs
+++ examples/echo.exs
@@ -10,27 +10,27 @@ Logger.debug("Upgrading to WebSocket protocol on /")
 
 message = receive(do: (message -> message))
 {:ok, conn, [{:status, ^ref, status}, {:headers, ^ref, resp_headers}, {:done, ^ref}]} =
-  Mint.HTTP.stream(conn, message)
+  Mint.WebSocket.stream(conn, message)
 {:ok, conn, websocket} = Mint.WebSocket.new(conn, ref, status, resp_headers)
 Logger.debug("WebSocket established")
 
 frame = {:text, "Rock it with Mint.WebSocket"}
 Logger.debug("Sending frame #{inspect(frame)}")
 {:ok, websocket, data} = Mint.WebSocket.encode(websocket, frame)
-{:ok, conn} = Mint.HTTP.stream_request_body(conn, ref, data)
+{:ok, conn} = Mint.WebSocket.stream_request_body(conn, ref, data)
 
 message = receive(do: (message -> message))
-{:ok, conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, message)
+{:ok, conn, [{:data, ^ref, data}]} = Mint.WebSocket.stream(conn, message)
 {:ok, websocket, frames} = Mint.WebSocket.decode(websocket, data)
 Logger.debug("Received frames #{inspect(frames)}")
 
 frame = :close
 Logger.debug("Sending frame #{inspect(frame)}")
 {:ok, websocket, data} = Mint.WebSocket.encode(websocket, frame)
-{:ok, conn} = Mint.HTTP.stream_request_body(conn, ref, data)
+{:ok, conn} = Mint.WebSocket.stream_request_body(conn, ref, data)
 
 message = receive(do: (message -> message))
-{:ok, conn, [{:data, ^ref, data}]} = Mint.HTTP.stream(conn, message)
+{:ok, conn, [{:data, ^ref, data}]} = Mint.WebSocket.stream(conn, message)
 {:ok, websocket, frames} = Mint.WebSocket.decode(websocket, data)
 Logger.debug("Received frames #{inspect(frames)}")
 
